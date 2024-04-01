545b869f9a0dd31e64f4a7cdbc17a8c0169476ea
ZeratoXD <ivan_cassio_dias@hotmail.com>
2024-04-01 11:26:42 -0300

    main

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -2,7 +2,7 @@ package com.generation.blogpessoal.controller;
 
 import java.util.List;
 
-import org.apache.el.stream.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/generation/blogpessoal/controller/TemaController.java
+++ src/main/java/com/generation/blogpessoal/controller/TemaController.java
@@ -1,8 +1,8 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
